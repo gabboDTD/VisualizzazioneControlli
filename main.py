8254583ecb8b1714e4568e9d45607cd221fd860e
gabboDTD <gabriele.spina@teamdigitale.governo.it>
2024-07-24 18:45:41 +0200

    aggiornati nomi colonne e valori ammissibili

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def generate_sample_data():
 
     # Define the columns and rows
     columns = [
-        # "Contratto tra SA e SR", 
-        # "Determina di affidamento / di aggiudicazione / ordine di servizio", 
-        # "Proposta commerciale SR accettata da SA", 
-        # "Documento di stipula MEPA", 
-        # "Convenzione / Accordo", 
-        "Checklist asseverazione tecnica", 
-        # "Certificato Regolare Esecuzione", 
-        # "Allegato 5"        
+        "Stato_Contratto_SA_SR", 
+        "Stato_Determina_Affidamento_Aggiudicazione_Servizio", 
+        "Stato_Proposta_Commerciale", 
+        "Stato_Documento_Stipula_MEPA", 
+        "Stato_Convenzione_Accordo", 
+        "Stato_Checklist_Asseverazione", 
+        "Stato_Certificato_Regolare_Esec", 
+        "Stato_Allegato_5"        
     ]
     indices = ['CND_141SCU0422X_015254',
                 'CND_141SCU1222X_079364',
@@ -31,16 +31,16 @@ def generate_sample_data():
 
     # Create a fixed DataFrame
     data = [
-        ['Documento OK',            ],
-        ['Errori nel documento',    ],
-        ['Documento mancante',      ],
-        ['Documento OK',            ],
-        ['Errori nel documento',    ],
-        ['Documento mancante',      ],
-        ['Documento OK',            ],
-        ['Documento mancante',      ],
-        ['Documento OK',            ],
-        ['Errori nel documento',    ]
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento valido',  'Controllo non eseguito', 'Controllo non eseguito'  ],
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento errato',  'Controllo non eseguito', 'Controllo non eseguito'  ],
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento assente', 'Controllo non eseguito', 'Controllo non eseguito'  ],
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento valido',  'Controllo non eseguito', 'Controllo non eseguito'  ],
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento errato',  'Controllo non eseguito', 'Controllo non eseguito'  ],
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento assente', 'Controllo non eseguito', 'Controllo non eseguito'  ],
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento valido',  'Controllo non eseguito', 'Controllo non eseguito'  ],
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento assente', 'Controllo non eseguito', 'Controllo non eseguito'  ],
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento valido',  'Controllo non eseguito', 'Controllo non eseguito'  ],
+        ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento errato',  'Controllo non eseguito', 'Controllo non eseguito'   ]
     ]
     # Populate the DataFrame with random values 0, 1, 2
     df = pd.DataFrame(data, index=indices[0:len(data)], columns=columns)
@@ -50,11 +50,11 @@ def generate_sample_data():
 def generate_specific_data_checklist():
     # Define the columns and rows
     columns = [
-        "CUP",
-        "Presenza della firma dell'asseveratore",
-        "Corrispondenza dell’Anagrafica del SA con quanto scritto nell’Allegato 5 / in piattaforma",
-        "Compilazione di tutti i campi della checklist (con la “x” su “positivo” o “n/a”)",
-        "Esito positivo della verifica di conformità tecnica del progetto"
+        "Stato_CUP",
+        "Stato_Firma_Asseveratore",
+        "Stato_Anagrafica_SA",
+        "Stato_Compilazione_Checklist",
+        "Esito_Conformità_Tecnica"
     ]
 
     indices = ['CND_141SCU0422X_015254',
@@ -69,37 +69,41 @@ def generate_specific_data_checklist():
                 'CND_141SCU0622X_032775']
     
     data = [
-        ['OK',          'OK',           'OK',           'OK',           'OK',],
-        ['Assente',     'Assente',      'Assente',      'OK',           'OK',],
-        ['No documento','No documento', 'No documento', 'No documento', 'No documento',],
-        ['OK',          'OK',           'OK',           'OK',           'OK',],
-        ['OK',          'Controllare',  'OK',           'OK',           'OK',],
-        ['No documento','No documento', 'No documento', 'No documento', 'No documento',],
-        ['OK',          'p7m',          'OK',           'OK',           'OK',],
-        ['No documento','No documento', 'No documento', 'No documento', 'No documento',],
-        ['OK',          'OK',           'OK',           'OK',           'OK',],
-        ['OK',          'Controllare',  'OK',           'Errore',       'OK',],
+        ['Codice corretto',     'Firma presente',       'Dati corretti,',           'Compilazione corretta',    'Positivo',],
+        ['Codice errato',       'Firma assente',        'Dati non corrispondenti',  'Compilazione corretta',    'Positivo',],
+        ['Documento assente',   'Documento assente',    'Documento assente',        'Documento assente',        'Documento assente',],
+        ['Codice corretto',     'Firma presente',       'Dati corretti,',           'Compilazione corretta',    'Positivo',],
+        ['Codice corretto',     'Verifica manuale',     'Dati corretti,',           'Compilazione corretta',    'Positivo',],
+        ['Documento assente',   'Documento assente',    'Documento assente',        'Documento assente',        'Documento assente',],
+        ['Verifica manuale',    'Documento p7m',        'Dati corretti,',           'Compilazione corretta',    'Positivo',],
+        ['Documento assente',   'Documento assente',    'Documento assente',        'Documento assente',        'Documento assente',],
+        ['Codice corretto',     'Firma presente',       'Dati corretti,',           'Compilazione corretta',    'Positivo',],
+        ['Codice corretto',     'Controllare',          'Dati corretti,',           'Compilazione errata',      'Positivo',],
     ]
 
     # Populate the DataFrame with random values 0, 1, 2
     df = pd.DataFrame(data, index=indices[0:len(data)], columns=columns)
     return df
 
+ 
+
+
+
 # Function to color the cells
 def color_cells(val):
     color = 'white'
-    if val in ['No documento']:
+    if val in ['Controllo non eseguito',]:
         color = 'blue'    
-    if val in ['Documento OK', 'OK', 'p7m']:
+    if val in ['Documento valido', 'Codice corretto', 'Firma presente', ' Documento p7m', 'Dati corretti', 'Compilazione corretta', 'Positivo']:
         color = 'lightgreen'
-    elif val in ['Errori nel documento','Controllare','Errore']:
+    elif val in ['Documento errato', 'Codice errato', 'Verifica manuale', 'Dati non corrispondenti', 'Compilazione errata', 'Negativo',]:
         color = 'orange'
-    elif val in ['Documento mancante','Assente']:
+    elif val in ['Documento assente', 'Codice assente', 'Firma assente', 'Campo nullo']:
         color = 'red'
     return f'background-color: {color}'
 
 # Path for the Excel file
-excel_file_path = 'sample_index.xlsx'
+excel_file_path = 'candidature_lavorate.xlsx'
 
 # Load or generate the data
 df = generate_sample_data()
@@ -135,7 +139,7 @@ if candidatura_search and column_search:
             # Check if a specific cell is clicked and display df_checklist
             if candidatura_search in df.index and column_search in df.columns:
                 selected_value = df.loc[candidatura_search, column_search]
-                if selected_value in ['Documento mancante', 'Errori nel documento', 'Documento OK']:
+                if selected_value in ['Documento assente', 'Documento errato', 'Documento valido']:
                     st.write(f"Dettagli per la candidatura '{candidatura_search}' e documento '{column_search}':")              
                     st.dataframe(df_checklist.loc[[candidatura_search]].style.applymap(color_cells))
                     # Button to append the index to the Excel file
