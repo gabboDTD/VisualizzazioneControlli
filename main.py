d9fe99f54229700396d49f0629e8ca53987e38ce
gabboDTD <gabriele.spina@teamdigitale.governo.it>
2024-07-29 13:40:06 +0200

    Aggiunta barra laterale per ricerca

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 
 # Sample DataFrame generation
 def generate_sample_data():
-
     # Define the columns and rows
     columns = [
         "Stato_Contratto_SA_SR", 
@@ -18,16 +17,18 @@ def generate_sample_data():
         "Stato_Certificato_Regolare_Esec", 
         "Stato_Allegato_5"        
     ]
-    indices = ['CND_141SCU0422X_015254',
-                'CND_141SCU1222X_079364',
-                'CND_144COM0422X_014068',
-                'CND_131COM1022X_082020',
-                'CND_141SCU1222X_079707',
-                'CND_141SCU1222X_079604',
-                'CND_131COM1022X_068828',
-                'CND_141SCU0622X_059144',
-                'CND_131COM1022X_081830',
-                'CND_141SCU0622X_032775']
+    indices = [
+        'CND_141SCU0422X_015254',
+        'CND_141SCU1222X_079364',
+        'CND_144COM0422X_014068',
+        'CND_131COM1022X_082020',
+        'CND_141SCU1222X_079707',
+        'CND_141SCU1222X_079604',
+        'CND_131COM1022X_068828',
+        'CND_141SCU0622X_059144',
+        'CND_131COM1022X_081830',
+        'CND_141SCU0622X_032775'
+    ]
 
     # Create a fixed DataFrame
     data = [
@@ -42,7 +43,6 @@ def generate_sample_data():
         ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento valido',  'Controllo non eseguito', 'Controllo non eseguito'  ],
         ['Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Controllo non eseguito', 'Documento errato',  'Controllo non eseguito', 'Controllo non eseguito'   ]
     ]
-    # Populate the DataFrame with random values 0, 1, 2
     df = pd.DataFrame(data, index=indices[0:len(data)], columns=columns)
 
     return df
@@ -57,42 +57,43 @@ def generate_specific_data_checklist():
         "Esito_Conformità_Tecnica"
     ]
 
-    indices = ['CND_141SCU0422X_015254',
-                'CND_141SCU1222X_079364',
-                'CND_144COM0422X_014068',
-                'CND_131COM1022X_082020',
-                'CND_141SCU1222X_079707',
-                'CND_141SCU1222X_079604',
-                'CND_131COM1022X_068828',
-                'CND_141SCU0622X_059144',
-                'CND_131COM1022X_081830',
-                'CND_141SCU0622X_032775']
+    indices = [
+        'CND_141SCU0422X_015254',
+        'CND_141SCU1222X_079364',
+        'CND_144COM0422X_014068',
+        'CND_131COM1022X_082020',
+        'CND_141SCU1222X_079707',
+        'CND_141SCU1222X_079604',
+        'CND_131COM1022X_068828',
+        'CND_141SCU0622X_059144',
+        'CND_131COM1022X_081830',
+        'CND_141SCU0622X_032775'
+    ]
     
     data = [
         ['Codice corretto',     'Firma presente',       'Dati corretti',           'Compilazione corretta',    'Positivo',],
         ['Codice errato',       'Firma assente',        'Dati non corrispondenti',  'Compilazione corretta',    'Positivo',],
         ['Documento assente',   'Documento assente',    'Documento assente',        'Documento assente',        'Documento assente',],
-        ['Codice corretto',     'Firma presente',       'Dati corretti,',           'Compilazione corretta',    'Positivo',],
-        ['Codice corretto',     'Verifica manuale',     'Dati corretti,',           'Compilazione corretta',    'Positivo',],
+        ['Codice corretto',     'Firma presente',       'Dati corretti',           'Compilazione corretta',    'Positivo',],
+        ['Codice corretto',     'Verifica manuale',     'Dati corretti',           'Compilazione corretta',    'Positivo',],
         ['Documento assente',   'Documento assente',    'Documento assente',        'Documento assente',        'Documento assente',],
-        ['Verifica manuale',    'Documento p7m',        'Dati corretti,',           'Compilazione corretta',    'Positivo',],
+        ['Verifica manuale',    'Documento p7m',        'Dati corretti',           'Compilazione corretta',    'Positivo',],
         ['Documento assente',   'Documento assente',    'Documento assente',        'Documento assente',        'Documento assente',],
-        ['Codice corretto',     'Firma presente',       'Dati corretti,',           'Compilazione corretta',    'Positivo',],
-        ['Codice corretto',     'Controllare',          'Dati corretti,',           'Compilazione errata',      'Positivo',],
+        ['Codice corretto',     'Firma presente',       'Dati corretti',           'Compilazione corretta',    'Positivo',],
+        ['Codice corretto',     'Controllare',          'Dati corretti',           'Compilazione errata',      'Positivo',],
     ]
 
-    # Populate the DataFrame with random values 0, 1, 2
     df = pd.DataFrame(data, index=indices[0:len(data)], columns=columns)
     return df
 
 # Function to color the cells
 def color_cells(val):
     color = 'white'
-    if val in ['Controllo non eseguito',]:
+    if val in ['Controllo non eseguito']:
         color = 'blue'    
-    if val in ['Documento valido', 'Codice corretto', 'Firma presente', ' Documento p7m', 'Dati corretti', 'Compilazione corretta', 'Positivo']:
+    if val in ['Documento valido', 'Codice corretto', 'Firma presente', 'Documento p7m', 'Dati corretti', 'Compilazione corretta', 'Positivo']:
         color = 'lightgreen'
-    elif val in ['Documento errato', 'Codice errato', 'Verifica manuale', 'Dati non corrispondenti', 'Compilazione errata', 'Negativo',]:
+    elif val in ['Documento errato', 'Codice errato', 'Verifica manuale', 'Dati non corrispondenti', 'Compilazione errata', 'Negativo']:
         color = 'orange'
     elif val in ['Errore nel controllo']:
         color = 'yellow'
@@ -105,27 +106,29 @@ excel_file_path = 'candidature_lavorate.xlsx'
 
 # Load or generate the data
 df = generate_sample_data()
-
 df_checklist = generate_specific_data_checklist()
 
 # Streamlit App
 st.title('Matrice dei controlli formali')
 
 st.write("""
-Questa app visualizza una tabella in cui ogni riga e' una candidatura in asseverazione formale,
-ogni colonna e' un documento da controllare, e ogni cella contiene un colore che indica se i controlli sul documento sono OK,
-se manca il documento (rosso), o se il documento continene errori (arancione)
+Questa app visualizza una tabella in cui ogni riga è una candidatura in asseverazione formale,
+ogni colonna è un documento da controllare, e ogni cella contiene un colore che indica se i controlli sul documento sono OK,
+se manca il documento (rosso), o se il documento contiene errori (arancione)
 """)
+
+# Sidebar for search inputs and rerun button
+st.sidebar.title("Ricerca Candidature")
+candidatura_search = st.sidebar.text_input('Cerca il nome della candidatura')
+column_search = st.sidebar.selectbox('Seleziona il nome del documento', df.columns)
+if st.sidebar.button('Rerun'):
+    st.experimental_rerun()
 
 # Check if the Excel file exists and remove rows from df that are in existing_data
 if os.path.exists(excel_file_path):
     existing_data = pd.read_excel(excel_file_path, header=None)
     existing_indices = existing_data[0].tolist()
     df = df.drop(existing_indices, errors='ignore')
-
-# Add search boxes to find the specific candidatura and column
-candidatura_search = st.text_input('Cerca il nome della candidatura')
-column_search = st.selectbox('Seleziona il nome del documento', df.columns)
 
 if candidatura_search and column_search:
     if column_search in df.columns:
@@ -153,10 +156,9 @@ if candidatura_search and column_search:
                             
                             # Write the data to Excel
                             all_data.to_excel(excel_file_path, index=False, header=False)
-                            st.success(f"La candidatura '{candidatura_search}' e' stata salvata su {excel_file_path} e non verra' piu' presentata!")
+                            st.success(f"La candidatura '{candidatura_search}' è stata salvata su {excel_file_path} e non verrà più presentata!")
                         except Exception as e:
                             st.error(f"Error writing to Excel: {e}")
-
         else:
             st.write(f"Nessun risultato trovato per '{candidatura_search}'")
     else:
@@ -164,11 +166,6 @@ if candidatura_search and column_search:
 else:
     st.write("Inserisci il nome della candidatura e del documento per visualizzare la riga corrispondente.")
 
-# Button to rerun the script
-if st.button('Rerun'):
-    st.experimental_rerun()
-
 # Display the full dataframe
 st.write("Tabella completa:")
 st.dataframe(df.style.applymap(color_cells))
-
