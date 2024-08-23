4a56655e7c15829ac2940d1dad38adbf02c10aaa
gabboDTD <gabriele.spina@teamdigitale.governo.it>
2024-08-23 08:36:27 +0200

    aggiunte docstrings in front end code

--- streamlit_frontend/streamlit_app_v2.py
+++ streamlit_frontend/streamlit_app_v2.py
@@ -21,6 +21,15 @@ API_URL_KEY = 'API_URL'
 CONFIG_PATH_KEY = 'CONFIG_PATH'
 
 def color_cells(val):
+    """
+    Apply background color to dataframe cells based on their value.
+
+    Args:
+        val (str): The value in the dataframe cell.
+
+    Returns:
+        str: The CSS style for the background color.
+    """    
     color_mapping = {
         'Documento non supportato': BLUE,
         'Controllo non supportato': BLUE,
@@ -48,11 +57,29 @@ def color_cells(val):
     return f'background-color: {color_mapping.get(val, "white")}'
 
 def get_api_url(endpoint):
+    """
+    Construct the full API URL based on the endpoint provided.
+
+    Args:
+        endpoint (str): The API endpoint.
+
+    Returns:
+        str: The full API URL.
+    """    
     base_url = os.getenv(API_URL_KEY)
     return f"{base_url}{endpoint}"
 
 @st.cache_data(ttl=3600)
 def fetch_documents(path):
+    """
+    Fetch the base64 encoded content of a document from the backend.
+
+    Args:
+        path (str): The document path to fetch.
+
+    Returns:
+        str: The base64 encoded content of the document or None if an error occurs.
+    """    
     api_url = get_api_url(f'document/{path}')
     response = requests.get(api_url)
     try:
@@ -65,6 +92,12 @@ def fetch_documents(path):
 
 @st.cache_data(ttl=3600)  # Set Time to live
 def fetch_data():
+    """
+    Fetch candidature data from the backend.
+
+    Returns:
+        list: A list of unique candidature IDs or None if an error occurs.
+    """    
     api_url = get_api_url(f'data')
     response = requests.get(api_url)
     try:
@@ -80,6 +113,15 @@ def fetch_data():
 
 @st.cache_data(ttl=3600)  # Set time to live
 def fetch_data2(id_candidatura):
+    """
+    Fetch detailed data for a specific candidature from the backend.
+
+    Args:
+        id_candidatura (str): The ID of the candidature to fetch.
+
+    Returns:
+        dict: The detailed data for the candidature or None if an error occurs.
+    """    
     api_url = get_api_url(f'detail/{id_candidatura}')
     response = requests.get(api_url)
     try:
@@ -93,6 +135,15 @@ def fetch_data2(id_candidatura):
 
 # Function to visualize the document
 def visualize_document(paths3):
+    """
+    Visualize a PDF document retrieved from the backend.
+
+    Args:
+        paths3 (str): The S3 path of the document to visualize.
+
+    Returns:
+        None
+    """    
     base64_pdf = fetch_documents(paths3)
     if base64_pdf:
         st.success("File read successfully!")
@@ -103,6 +154,15 @@ def visualize_document(paths3):
         st.error("Failed to fetch document from API")
 
 def prepro(query_data):
+    """
+    Preprocess the query data to extract documents and checklist information.
+
+    Args:
+        query_data (list): The raw data returned from the backend query.
+
+    Returns:
+        tuple: Two DataFrames, one for the documents and one for the checklist.
+    """    
     df_documents = pd.DataFrame([
         {
             'documentClass': entry['documentClass'],
@@ -134,6 +194,15 @@ def prepro(query_data):
 
 # Function to read and decrypt p7m file
 def read_p7m(file_path):
+    """
+    Read and extract the content of a p7m file.
+
+    Args:
+        file_path (str): The file path of the p7m file to read.
+
+    Returns:
+        bytes: The extracted content of the p7m file.
+    """    
     with open(file_path, "rb") as f:
         data = f.read()
     
@@ -147,15 +216,36 @@ def read_p7m(file_path):
 
 # Function to read a PDF file from bytes and convert it to base64
 def read_pdf(file_bytes):
+    """
+    Convert a PDF file from bytes to a base64 encoded string.
+
+    Args:
+        file_bytes (bytes): The bytes of the PDF file.
+
+    Returns:
+        str: The base64 encoded string of the PDF file.
+    """    
     return base64.b64encode(file_bytes).decode('utf-8')
 
 def save_config():
+    """
+    Save the current configuration to a YAML file.
+
+    Returns:
+        None
+    """    
     config_path = os.getenv(CONFIG_PATH_KEY, 'config.yaml')
     with open(config_path, 'w', encoding='utf-8') as file:
         yaml.dump(config, file, default_flow_style=False)
 
 @st.cache_resource
 def load_config():
+    """
+    Load the configuration from a YAML file.
+
+    Returns:
+        dict: The loaded configuration.
+    """    
     config_path = os.getenv(CONFIG_PATH_KEY, 'config.yaml')
     with open(config_path, 'r', encoding='utf-8') as file:
         return yaml.load(file, Loader=SafeLoader)
